e5475feabded1d0ceb648c451ab42290294d11f2
Jeremy Taylor <tay16036@byui.edu>
2017-07-10 00:07:24 -0600

    7/10/2017 Created Model, View, and Control Layers up through migration section of tutorial.

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+		private readonly SchoolContext _context;
+
+		public HomeController(SchoolContext context)
+		{
+			_context = context;
+		}
+			public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+		public async Task<ActionResult> About()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Students
+				group student by student.EnrollmentDate into dateGroup
+				select new EnrollmentDateGroup()
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+			return View(await data.AsNoTracking().ToListAsync());
+		}
 
-            return View();
-        }
-
-        public IActionResult Contact()
+		public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
